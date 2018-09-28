23bc6cf1f392932015edf50df766cc1c9d2f6ae9
vonslience <vorjohnl@gmail.com>
2018-09-28 17:20:50 +0800

    "[^]"

--- ultimate/src/main/main.go
+++ ultimate/src/main/main.go
@@ -4,13 +4,9 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
-
 func main() {
-
 	gin.SetMode(gin.DebugMode)
-
     app := gin.Default()
-
     route := app.Group("/api/v1")
     {
     	route.GET("/user", func(context *gin.Context) {
@@ -18,15 +14,12 @@ func main() {
 				"response":context.Query("name"),
 			})
 		})
-
     	route.POST("/user", func(context *gin.Context) {
 			context.JSON(http.StatusOK,gin.H{
 				"response":context.PostForm("name"),
 			})
 		})
-
 	}
-
     app.Run(":7074")
 
 }
